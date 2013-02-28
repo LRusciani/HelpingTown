3f88d4e17a06956c1943f0ea1a092fc9b0cf4f11
turbotom <s0718702@monmouth.edu>
2013-02-27 20:57:32 -0500

    Create testerStyle.php

---
+++ testerStyle.php
@@ -0,0 +1,49 @@
+body {
+  background:white;
+	color:black;
+	font:0.8em Arial, Helvetica, "bitstream vera sans", sans-serif;
+}
+
+
+#header-cont {
+    width:100%;
+    position:fixed;
+    top:0px;
+    background-color:#9ACD32;
+    opacity: 0.6;
+    filter:Alpha(opacity=60); /* IE8 and earlier */
+    height:25px;
+    
+    
+}
+#header {
+    height:10px;
+    background:#F0F0F0;
+    border:1px solid #CCC;
+    width:100%;
+    margin:0px;
+}
+
+
+#header-cont a{
+	margin:5px;
+	font-weight: bold;
+	color:blue;
+	text-decoration: none;
+	font-size: 20;
+}
+
+
+#header-cont a:hover{
+	opacity: 1.0;
+	color: #49AAEE;
+}
+
+#header-cont li {
+	float: right;
+	font-size: 10;
+	margin-top: 5px;
+	margin-right:10px;
+}
+
+
